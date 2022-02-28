2d2e2f5a86e3188588918a9d7a701ca010900e5e
Julio Meza <juliomeza@gmail.com>
2022-02-27 20:52:12 -0500

    JS

--- static/js/app.js
+++ static/js/app.js
@@ -19,18 +19,18 @@ function buildTable(data) {
             }
         );
     });
-};
+}
 
 function handleClick(){
     // Grab the datetime value from the filter
     let date = d3.select('#datetime').property('value');
-    let filterData = tableData;
+    let filteredData = tableData;
 
     // Check to see if a date was entered and filter the data using that date.
     if (date) {
         // Apply `filter` to the table data to only keep the
         // rows where the `datetime` value matches the filter value
-        filterData = filterData.filter(row => row.datetime === date);
+        filteredData = filteredData.filter(row => row.datetime === date);
     };
 
     // Rebuild the table using the filtered data
